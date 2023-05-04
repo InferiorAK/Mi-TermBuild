b56fac3e4085f2cfbe317159c0ca2d7ba27e31ee
Mi Taseen <114951279+InferiorAK@users.noreply.github.com>
2023-05-05 00:53:20 +0600

    Update funcs.sh

--- main/funcs.sh
+++ main/funcs.sh
@@ -215,9 +215,14 @@ reload(){
 
 # main menu
 menu(){
-	active_theme=$(cat res/.active_theme)
-	active_font=$(cat res/.active_font)
-	active_key=$(cat res/.active_key)
+	if [ -e res/.* ] ; then
+		active_theme=$(cat res/.active_theme)
+		active_font=$(cat res/.active_font)
+		active_key=$(cat res/.active_key)
+	else
+		:
+	fi
+
 	clear
 	banner
 	echo -e "
